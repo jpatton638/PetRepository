5acbcc16f9e9ee4d5a7880efbb7ebd79ee5f54e2
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-02-05 14:32:01 +0000

    class creation

--- src/main/scala/App/PetRepository.scala
+++ src/main/scala/App/PetRepository.scala
@@ -5,4 +5,10 @@ package App
   */
 object PetRepository extends App {
 
+  class Pet (name : String)
+
+  def all() : List[Pet] = Nil
+  def findByName(name : String) : Option[Pet] = None
+  def dogs() : List[Pet] = Nil
+  def cats() : List[Pet] = Nil
 }
