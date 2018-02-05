fb93647d13aeac983fac2ca7c8624577bf1f3675
john.patton <john.patton@digital.hmrc.gov.uk>
2018-02-05 14:39:31 +0000

    Object structure complete

--- src/main/scala/App/PetRepository.scala
+++ src/main/scala/App/PetRepository.scala
@@ -8,14 +8,16 @@ import scala.collection.mutable.ArrayBuffer
 object PetRepository extends App {
   
   class Pet (name : String)
-  object Pets = new ArrayBuffer[Pet]()
+  private val pets = new ArrayBuffer[Pet]()
 
   def all() : List[Pet] = Nil
   def findByName(name : String) : Option[Pet] = None
   def dogs() : List[Pet] = Nil
   def cats() : List[Pet] = Nil
   def other(): List[Pet] = Nil
-  def add(pet: Pet): List[Pet]
-  def removeByName(name: String): List[Pet]
-  def update(pet: Pet): List[Pet]
+  def add(pet: Pet): List[Pet] = Nil
+  def removeByName(name: String): List[Pet] = Nil
+  def update(pet: Pet): List[Pet] = Nil
 }
+
+
