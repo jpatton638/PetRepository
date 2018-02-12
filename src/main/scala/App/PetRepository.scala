df328bc0cf82573643e372fa931a29e817072c1e
jpatton638 <jpatton638@gmail.com>
2018-02-12 14:21:32 +0000

    Split work

--- src/main/scala/App/PetRepository.scala
+++ src/main/scala/App/PetRepository.scala
@@ -10,10 +10,13 @@ object PetRepository extends App {
   class Pet (name : String)
   private val pets = new ArrayBuffer[Pet]()
 
+  // Andy
   def all() : List[Pet] = Nil
   def findByName(name : String) : Option[Pet] = None
   def dogs() : List[Pet] = Nil
   def cats() : List[Pet] = Nil
+
+  // John
   def other(): List[Pet] = Nil
   def add(pet: Pet): List[Pet] = Nil
   def removeByName(name: String): List[Pet] = Nil
