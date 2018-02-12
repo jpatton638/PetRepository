df25dd32f711bf9792bf42ffb9b8b4dedde3c9e9
jpatton638 <jpatton638@gmail.com>
2018-02-12 17:57:05 +0000

    Still working on update

--- src/main/scala/App/PetRepository.scala
+++ src/main/scala/App/PetRepository.scala
@@ -22,20 +22,42 @@ object PetRepository extends App {
   // John
   def other(): List[Pet] = {
     val filtered = pets.filter(p => p.isInstanceOf[Pet])
+    println(filtered.toList)
     filtered.toList
   }
   def add(pet: Pet*): List[Pet] = {
     pet.map(p => pets += p)
+    println(pets.toList)
     pets.toList
   }
 
   def removeByName(name: String): List[Pet] = {
     val filtered = pets.filter(p => p.name == name)
+    println(filtered.toList)
     filtered.toList
   }
   def update(pet: Pet): List[Pet] = {
-    ???
+    val petsList = pets.toList
+      petsList.map(p =>
+      if (p.name == pet.name) {
+        pets -= p
+        pets += pet
+      }
+    )
+    println(pets.toList)
+    pets.toList
   }
+
+  val pet1 = Pet("Harry")
+  val pet2 = Pet("James")
+  val pet3 = Dog("Bogdan")
+  val pet4 = Cat("Lawrence")
+  val pet5 = Pet("Matty")
+
+  add(pet1, pet2, pet3, pet4, pet5)
+  other()
+  removeByName("Bogdan")
+  update(pet2)
 }
 
 
