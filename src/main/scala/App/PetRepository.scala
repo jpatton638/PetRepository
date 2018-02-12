620a495ad9598eb08cdadbc596dfacbb06e7fad0
jpatton638 <jpatton638@gmail.com>
2018-02-12 18:15:26 +0000

    All methods for John implemented

--- src/main/scala/App/PetRepository.scala
+++ src/main/scala/App/PetRepository.scala
@@ -6,10 +6,10 @@ import scala.collection.mutable.ArrayBuffer
   * Created by john on 05/02/18.
   */
 object PetRepository extends App {
-  
+
   case class Pet (name : String)
-  case class Dog(override val name: String) extends Pet(name)
-  case class Cat(override val name: String) extends Pet(name)
+  case class Dog(name: String)
+  case class Cat(name: String)
 
   private val pets = new ArrayBuffer[Pet]()
 
@@ -32,7 +32,7 @@ object PetRepository extends App {
   }
 
   def removeByName(name: String): List[Pet] = {
-    val filtered = pets.filter(p => p.name == name)
+    val filtered = pets.filter(p => p.name != name)
     println(filtered.toList)
     filtered.toList
   }
@@ -50,8 +50,8 @@ object PetRepository extends App {
 
   val pet1 = Pet("Harry")
   val pet2 = Pet("James")
-  val pet3 = Dog("Bogdan")
-  val pet4 = Cat("Lawrence")
+  val pet3 = Pet("Bogdan")
+  val pet4 = Pet("Lawrence")
   val pet5 = Pet("Matty")
 
   add(pet1, pet2, pet3, pet4, pet5)
