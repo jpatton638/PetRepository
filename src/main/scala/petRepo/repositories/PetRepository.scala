0e98f5778b7ddc96ecd3109c395c8535476d1d0e
Andrew Armstrong <andrew.armstrong@digital.hmrc.gov.uk>
2018-02-14 11:02:31 +0000

    add methods

--- src/main/scala/petRepo/repositories/PetRepository.scala
+++ src/main/scala/petRepo/repositories/PetRepository.scala
@@ -9,42 +9,28 @@ import scala.collection.mutable.ArrayBuffer
   */
 object PetRepository {
 
+  
+
   private val _pets : ArrayBuffer[Pet] = new ArrayBuffer[Pet]()
 
   def all() : List[Pet] = _pets.toList
 
-  def findByName(name: String) : Option[Pet] = {
-    //    val filtered = _pets.filterNot(p => p.name == name)
-    //    filtered.headOption
-    _pets.find(p => p.name == name)
+  def findByName(name: String) : Option[Pet] = _pets.find(p => p.name == name)
+
+
+  def dogs : List[Dog] = {
+    _pets.toList.flatMap { case d @ Dog(_, _) => Some(d)
+    case _ => None
+    }
   }
 
-  def dogs() : List[Pet] = _pets.toList
-
-  def cats() : List[Pet] = _pets.toList
+  def cats : List[Cat] = {
+    _pets.toList.flatMap { case c @ Cat(_, _) => Some(c)
+    case _ => None
+    }
+  }
 
   def other() : List[Pet] = _pets.toList
 
-  def add(pet : Pet*) : List[Pet] = {
-    //  _pets.++=(pet)
-    pet.foreach(p => _pets.+=(p))
-    all
-  }
-
-  def removeByName(name : String) : List[Pet] = {
-    // find the pet you want to remove by name
-    // remove that pet from _pets list
-    // reassign the _pets with the new filtered list
-    // _pets = filtered
-    // return the new list with all()
-    _pets.toList
-  }
-
-  def update(pet: Pet) : List[Pet] = {
-    // find the index of the pet is it at position 1, 2 or 3 for example?
-    // call _pets.update(index, pet)
-    // all()
-    _pets.toList
-  }
 
 }
